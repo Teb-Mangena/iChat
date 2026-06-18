ae5e7139c27fc971f5c831c0f2c0a292fad48dde
Tebatso <tebatsomangena8@gmail.com>
2026-06-18 12:57:02 +0200

    Add cron job for health check and update environment configuration
    - Implemented a cron job to send a GET request to the health endpoint every 14 minutes.
    - Added FRONTEND_URL to the environment configuration.
    - Updated CORS middleware to allow requests from the specified frontend URL.
    - Enhanced .gitignore and .dockerignore for better file management.
    - Created Dockerfile for multi-stage build of frontend and backend.

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -4,6 +4,7 @@ export const ENV = {
   PORT: process.env.PORT || 5050,
   NODE_ENV: process.env.NODE_ENV,
   MONGO_URI: process.env.MONGO_URI,
+  FRONTEND_URL: process.env.FRONTEND_URL,
   UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
   CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
@@ -11,6 +12,7 @@ export const ENV = {
 
 const requiredKeys = [
   "MONGO_URI",
+  "FRONTEND_URL",
   "UPSTASH_REDIS_REST_URL",
   "CLERK_SECRET_KEY",
   "CLERK_PUBLISHABLE_KEY",
