ef4588d717e700f5ce907d163e80e17180c65878
Tebatso <tebatsomangena8@gmail.com>
2026-06-21 13:47:56 +0200

    feat: integrate Hero UI components and update authentication flow
    - Added @heroui/react and @heroui/styles dependencies to package.json.
    - Imported Hero UI styles in index.css.
    - Updated AuthPage to use Hero UI Button for sign-in functionality, replacing the previous Clerk SignInButton.
    - Modified HomePage to clean up unused imports and maintain consistency.

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,13 +1,18 @@
-import { Show, SignInButton } from "@clerk/react"
+import { useClerk } from "@clerk/react"
+import { Button } from "@heroui/react"
 
 function AuthPage() {
+  const { openSignIn } = useClerk();
+
+  const handleClerk = () => {
+    openSignIn({ fallbackRedirectUrl: "/", forceRedirectUrl: "/" })
+  }
+
   return (
     <div>
-      <div>AuthPage</div>
+      <div>IChat your messaging web app</div>
 
-      <Show when="signed-out">
-        <SignInButton mode='modal' />
-      </Show>
+      <Button className="bg-purple-500 hover:bg-purple-600" onClick={handleClerk}>Continue</Button>
     </div>
   )
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,14 +1,11 @@
 import { Show, UserButton } from '@clerk/react'
 import LogOut from '../components/LogOut'
 
-
 function HomePage() {
-
 
   return (
     <>
       <div>HomePage</div>
-
       <header>
 
         <Show when="signed-in">
